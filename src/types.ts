2acd8e8b4f56fdb98dbe2c80e8811a7f73076b6d
Muhammad Khan <muhammadshoaibkhan706@gmail.com>
2021-05-02 22:46:59 +0500

    added typescript data types

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export interface Mars {
+  id: number;
+  sol: number;
+  camera: {
+    id: number;
+    name: string;
+    rover_id: number;
+    full_name: string;
+  };
+  img_src: string;
+  earth_date: string;
+  rover: {
+    id: number;
+    name: string;
+    landing_date: string;
+    launch_date: string;
+    status: string;
+  };
+}
+
+export interface MarsDataType {
+  photos: Array<Mars>;
+}
